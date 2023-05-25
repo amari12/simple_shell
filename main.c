7570161bbc63c0136e9da9fb75cb611a4ee3aa73
Amari <amarikotze@gmail.com>
2023-05-25 13:20:14 -0700

    authors

--- main.c
+++ main.c
@@ -15,18 +15,19 @@ int main(int argc __attribute__((unused)),
 		char *argv[] __attribute__((unused)))
 {
 	char *input;
-	/*char *exit_msg = "EOF\n...\n";*/
 	char *cmd;
 	char *args[ARGS_SIZE];
 	int exit_loop = 0, loops = 0; /*, result;*/
+	int og_stdout_fd, c, stdout_fd;
+	FILE *stdout_file;
 
 	if (isatty(STDIN_FILENO) == 1)
 	{ /*interactive mode*/
-		/*path = getenv("PATH");*/ /*get path*/
+		og_stdout_fd = dup(STDOUT_FILENO);
 		while (exit_loop != 1) /*shell loop*/
 		{
 			write(STDOUT_FILENO, "our shell>> ", 12);
-			fflush(stdout); /*flush output buffer*/
+			fflush(stdout);
 			input = get_input(); /*read input*/
 			/*get_input2(&input);*/ /*advanced*/
 			if (input != NULL)
@@ -36,17 +37,31 @@ int main(int argc __attribute__((unused)),
 				cmd = args[0];
 				if (handle_builtins(args) == 1) /*handle builtins*/
 					continue; /*env builtin or cd -> restart loop*/
-			/*	result =*/ 
+			/*	result =*/
 				forking(args, cmd); /*fork and exe child process*/
 				loops++;
-			/*	if (result != 0)*/
-			/*	{		*/
-			/*		write_error(args, loops);*/
-			/*	}*/
+				/*if (result != 0)*/
+				/*{*/
+				/*	write_error(args, loops);*/
+				/*}*/
+				/*print stdout - stdout.txt*/
+				stdout_fd = open("stdout.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+				dup2(stdout_fd, STDOUT_FILENO);
+				close(stdout_fd);
+				stdout_file = fopen("stdout.txt", "r");
+				if (stdout_file != NULL)
+				{
+					while ((c = fgetc(stdout_file)) != EOF)
+					{
+						putchar(c);
+					}
+					fclose(stdout_file);
+				}
+				dup2(og_stdout_fd, STDOUT_FILENO);
 			}
 			free(input);
-		} /*show prompt*/
-	/*	free(input);*/
+		}
+		close(og_stdout_fd);
 	}
 	/*else non interactive mode*/
 	return (0);
