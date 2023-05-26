63f5004b2161cedeffbdd1120371b246a6120ef9
Amari <amarikotze@gmail.com>
2023-05-26 11:38:18 -0700

    token

--- tokeniz.c
+++ tokeniz.c
@@ -13,40 +13,40 @@ char **_strtok1(char *str, char *d)
 	int i, j, k, m, numwords = 0;
 	char **s;
 
-	if (str == NULL || str[0] == 0) /*check input str*/
+	if (str == NULL || str[0] == 0)
 		return (NULL);
-	if (d == NULL) /*empty list for deliminators*/
+	if (!d)
 		d = " ";
-	/*Count the number of words in the input string based on the delimiters*/
 	for (i = 0; str[i] != '\0'; i++)
 		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
 			numwords++;
-	if (numwords == 0) /*no words found*/
+
+	if (numwords == 0)
 		return (NULL);
 	s = malloc((1 + numwords) * sizeof(char *));
-	if (s == NULL) /*malloc failed*/
+	if (!s)
 		return (NULL);
 	for (i = 0, j = 0; j < numwords; j++)
-	{ /*tokenise*/
-		while (is_delim(str[i], d)) /*skip delims*/
+	{
+		while (is_delim(str[i], d))
 			i++;
 		k = 0;
 		while (!is_delim(str[i + k], d) && str[i + k])
-			k++; /*cpy chars of word*/
+			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
-		if (s[j] != NULL) /*malloc failed*/
+		if (!s[j])
 		{
-			for (k = 0; k < j; k++) /*free prev*/
+			for (k = 0; k < j; k++)
 				free(s[k]);
 			free(s);
 			return (NULL);
 		}
 		for (m = 0; m < k; m++)
 			s[j][m] = str[i++];
-		s[j][m] = 0; /*add null terminator*/
+		s[j][m] = 0;
 	}
-	s[j] = NULL; /*add null ptr*/
-	return (s); /*return arr of char pointers -> words*/
+	s[j] = NULL;
+	return (s);
 }
 
 /**
