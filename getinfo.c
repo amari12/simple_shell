acb97a37bec32eb25ecd5175fa0ec0f4a127048f
Amari <amarikotze@gmail.com>
2023-05-26 16:09:07 -0700

    get info

--- getinfo.c
+++ getinfo.c
@@ -2,10 +2,13 @@
 
 /**
  * clear_inf - initializes inf_t struct
- * @inf: struct address
+ * @inf: struct ptr
+ * Return: void
  */
+
 void clear_inf(inf_t *inf)
 {
+	/*initialse struct*/
 	inf->arg = NULL;
 	inf->argv = NULL;
 	inf->path = NULL;
@@ -14,61 +17,75 @@ void clear_inf(inf_t *inf)
 
 /**
  * set_inf - initializes inf_t struct
- * @inf: struct address
- * @av: argument vector
+ * @inf: struct ptr
+ * @argv: argument vector
+ * Return: void
  */
-void set_inf(inf_t *inf, char **av)
+
+void set_inf(inf_t *inf, char **argv)
 {
 	int i = 0;
 
-	inf->fname = av[0];
+	inf->fname = argv[0];
+	/*check for additional args*/
 	if (inf->arg)
 	{
+		/*split input*/
 		inf->argv = strtow(inf->arg, " \t");
-		if (!inf->argv)
+		if (!inf->argv) /*tokenisation failed*/
 		{
-
+			/*memory allocation*/
 			inf->argv = malloc(sizeof(char *) * 2);
 			if (inf->argv)
-			{
+			{ /*assign arguments*/
 				inf->argv[0] = strdup(inf->arg);
 				inf->argv[1] = NULL;
 			}
 		}
+		/*count args*/
 		for (i = 0; inf->argv && inf->argv[i]; i++)
+		{
 			;
+		}
 		inf->argc = i;
-
+		/*Replace aliases with their corresponding values*/
 		replace_alias(inf);
+		/*Replace aliases with their corresponding values*/
 		replace_vars(inf);
 	}
 }
 
 /**
  * free_inf - frees inf_t struct fields
- * @inf: struct address
- * @all: true if freeing all fields
+ * @inf: struct ptr
+ * @all: true if freeing all successful
  */
+
 void free_inf(inf_t *inf, int all)
 {
+	/*free memory*/
 	ffree(inf->argv);
+	/*reset*/
 	inf->argv = NULL;
 	inf->path = NULL;
-	if (all)
+	if (all) /*flag = true*/
 	{
+		/*If inf->cmd_buf is not allocated (NULL)*/
 		if (!inf->cmd_buf)
 			free(inf->arg);
-		if (inf->env)
+		if (inf->env) /*inf->env is allocated (not NULL)*/
 			free_list(&(inf->env));
-		if (inf->history)
+		if (inf->history) /*inf->history is allocated (not NULL)*/
 			free_list(&(inf->history));
-		if (inf->alias)
+		if (inf->alias) /*inf->alias is allocated (not NULL)*/
 			free_list(&(inf->alias));
-		ffree(inf->environ);
+		ffree(inf->environ); /*environ*/
 			inf->environ = NULL;
 		bfree((void **)inf->cmd_buf);
+		/*If inf->readfd is greater than 2 (file descriptor)*/
 		if (inf->readfd > 2)
 			close(inf->readfd);
+		/*flush output buffer*/
 		_putchar(FLUSH);
 	}
 }
