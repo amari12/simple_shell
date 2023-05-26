ba806e25d2f9f4dffe7237d6fca34f776858f243
Amari <amarikotze@gmail.com>
2023-05-26 16:59:24 -0700

    parse

--- parser.c
+++ parser.c
@@ -2,86 +2,99 @@
 
 /**
  * is_cmd - determines if a file is an executable command
- * @inf: the inf struct
- * @path: path to the file
- *
- * Return: 1 if true, 0 otherwise
+ * @inf: struct
+ * @path: path to file
+ * Return: 1 true, 0 false
  */
+
 int is_cmd(inf_t *inf, char *path)
 {
 	struct stat st;
 
+	/*unused par*/
 	(void)inf;
-	if (!path || stat(path, &st))
-		return (0);
 
+	/*check*/
+	if (path == NULL || stat(path, &st))
+		return (0);
 	if (st.st_mode & S_IFREG)
 	{
-		return (1);
+		return (1); /*success*/
 	}
+	/*default*/
 	return (0);
 }
 
 /**
  * dup_chars - duplicates characters
  * @pathstr: the PATH string
- * @start: starting index
- * @stop: stopping index
- *
- * Return: pointer to new buffer
+ * @start: start index
+ * @stop: stop index
+ * Return: ptr to new buffer
  */
+
 char *dup_chars(char *pathstr, int start, int stop)
 {
 	static char buf[1024];
 	int i = 0, k = 0;
 
+	/*loop throug*/
 	for (k = 0, i = start; i < stop; i++)
+	{
 		if (pathstr[i] != ':')
 			buf[k++] = pathstr[i];
+	}
+
+	/*nul terminator*/
 	buf[k] = 0;
+	/*out*/
 	return (buf);
 }
 
 /**
  * find_path - finds this cmd in the PATH string
- * @inf: the inf struct
- * @pathstr: the PATH string
- * @cmd: the cmd to find
- *
- * Return: full path of cmd if found or NULL
+ * @inf: struct
+ * @pathstr: the PATH
+ * @cmd: cmd
+ * Return: full path of cmd if found || NULL
  */
+
 char *find_path(inf_t *inf, char *pathstr, char *cmd)
 {
 	int i = 0, curr_pos = 0;
 	char *path;
 
-	if (!pathstr)
+	if (pathstr == NULL) /*empty*/
 		return (NULL);
+
 	if ((strlen(cmd) > 2) && starts_with(cmd, "./"))
-	{
-		if (is_cmd(inf, cmd))
+	{ /*If the command starts with "./"*/
+		if (is_cmd(inf, cmd)) /*valid*/
 			return (cmd);
 	}
-	while (1)
+
+	while (1) /*loop*/
 	{
 		if (!pathstr[i] || pathstr[i] == ':')
 		{
 			path = dup_chars(pathstr, curr_pos, i);
-			if (!*path)
+			/*check:*/
+			if (!*path) /*empty*/
 				strcat(path, cmd);
 			else
 			{
 				strcat(path, "/");
 				strcat(path, cmd);
 			}
-			if (is_cmd(inf, path))
+			if (is_cmd(inf, path)) /*If constructed path= valid executable*/
 				return (path);
-			if (!pathstr[i])
-				break;
+			if (!pathstr[i]) /*end of path string*/
+				break; /*exit loop*/
 			curr_pos = i;
 		}
 		i++;
 	}
+	/*default*/
 	return (NULL);
 }
 
