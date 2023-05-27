c5a89b2a04ce45a980ffce0299945119dbf5db2c
Amari <amarikotze@gmail.com>
2023-05-26 17:22:23 -0700

    shell_loop

--- shell_loop.c
+++ shell_loop.c
@@ -1,61 +1,65 @@
 #include "shell.h"
 
 /**
- * hsh - main shell loop
- * @inf: the parameter & return inf struct
- * @av: the argument vector from main()
- *
- * Return: 0 on success, 1 on error, or error code
+ * loop - main shell loop
+ * @inf: struct
+ * @argv: argument vector from main()
+ * Return: 0 success, 1 error / error code
  */
 
-int loop(inf_t *inf, char **av)
+int loop(inf_t *inf, char **argv)
 {
 	ssize_t r = 0;
-	int builtin_ret = 0;
+	int output = 0;
 
-	while (r != -1 && builtin_ret != -2)
-	{
+	while (r != -1 && output != -2)
+	{ /*MAIN LOOP*/
 		clear_inf(inf);
+		/*prompt*/
 		if (is_interactive(inf))
 			_puts("$ ");
 		_eputchar(FLUSH);
-		r = get_input(inf);
+
+		r = get_input(inf); /*get user input*/
 		if (r != -1)
 		{
-			set_inf(inf, av);
-			builtin_ret = find_builtin(inf);
-			if (builtin_ret == -1)
+			set_inf(inf, argv);
+			output = find_builtin(inf);
+			if (output == -1)
 				find_cmd(inf);
 		}
-		else if (is_interactive(inf))
+		else if (is_interactive(inf)) /*interactive mode*/
 			_putchar('\n');
+
 		free_inf(inf, 0);
-	}
-	write_history(inf);
+	} /*end of loop*/
+	write_history(inf); /*write cmd to hist file*/
 	free_inf(inf, 1);
+	/*handle exit based on loop result*/
 	if (!is_interactive(inf) && inf->status)
 		exit(inf->status);
-	if (builtin_ret == -2)
+	if (output == -2)
 	{
 		if (inf->err_num == -1)
 			exit(inf->status);
 		exit(inf->err_num);
 	}
-	return (builtin_ret);
+	return (output);
 }
 
 /**
  * find_builtin - finds a builtin command
- * @inf: the parameter & return inf struct
- *
- * Return: -1 if builtin not found,
- *			0 if builtin executed successfully,
- *			1 if builtin found but not successful,
- *			-2 if builtin signals exit()
+ * @inf: struct
+ * Return: -1 builtin not found,
+ *	0 builtin executed successfully,
+ *	1 builtin found but not successful,
+ *	-2 builtin signals exit()
  */
+
 int find_builtin(inf_t *inf)
 {
-	int i, built_in_ret = -1;
+	int i, out = -1;
+	/*array of bi cmds*/
 	builtin_table builtintbl[] = {
 		{"exit", _myexit},
 		{"env", _myenv},
@@ -68,52 +72,65 @@ int find_builtin(inf_t *inf)
 		{NULL, NULL}
 	};
 
+	/*iterate throug*/
 	for (i = 0; builtintbl[i].type; i++)
+	{
 		if (strcmp(inf->argv[0], builtintbl[i].type) == 0)
 		{
 			inf->line_count++;
-			built_in_ret = builtintbl[i].func(inf);
+			/*Execute the corresponding built-in function*/
+			out = builtintbl[i].func(inf);
 			break;
 		}
-	return (built_in_ret);
+	}
+	/*output*/
+	return (out);
 }
 
 /**
  * find_cmd - finds a command in PATH
- * @inf: the parameter & return inf struct
- *
+ * @inf: struct
  * Return: void
  */
+
 void find_cmd(inf_t *inf)
 {
 	char *path = NULL;
 	int i, k;
 
 	inf->path = inf->argv[0];
+	/*Increment line count if linecount_flag is set*/
 	if (inf->linecount_flag == 1)
 	{
 		inf->line_count++;
 		inf->linecount_flag = 0;
 	}
+
+	/*Count the number of non-delimiter arguments*/
 	for (i = 0, k = 0; inf->arg[i]; i++)
+	{
 		if (!is_delim(inf->arg[i], " \t\n"))
 			k++;
-	if (!k)
+	}
+	if (!k) /*no args*/
 		return;
 
+	/*Find the executable path using the PATH*/
 	path = find_path(inf, _getenv(inf, "PATH="), inf->argv[0]);
 	if (path)
-	{
+	{ /*found path*/
 		inf->path = path;
 		fork_cmd(inf);
 	}
 	else
-	{
+	{ /*path is not found*/
 		if ((is_interactive(inf) || _getenv(inf, "PATH=")
 			|| inf->argv[0][0] == '/') && is_cmd(inf, inf->argv[0]))
+		{ /*If it is an absolute or relative path..*/
 			fork_cmd(inf);
+		}
 		else if (*(inf->arg) != '\n')
-		{
+		{ /*set the status to 127 and print an error message*/
 			inf->status = 127;
 			print_error(inf, "not found\n");
 		}
@@ -122,40 +139,44 @@ void find_cmd(inf_t *inf)
 
 /**
  * fork_cmd - forks a an exec thread to run cmd
- * @inf: the parameter & return inf struct
- *
+ * @inf: struct
  * Return: void
  */
+
 void fork_cmd(inf_t *inf)
 {
 	pid_t child_pid;
 
+	/*fork child process*/
 	child_pid = fork();
+
 	if (child_pid == -1)
-	{
-		/* TODO: PUT ERROR FUNCTION */
+	{ /*failed*/
 		perror("Error:");
 		return;
 	}
 	if (child_pid == 0)
-	{
+	{ /*child process*/
+		/*execute cmd*/
 		if (execve(inf->path, inf->argv, get_environ(inf)) == -1)
-		{
+		{ /*execve fails*/
 			free_inf(inf, 1);
 			if (errno == EACCES)
 				exit(126);
 			exit(1);
 		}
-		/* TODO: PUT ERROR FUNCTION */
 	}
 	else
-	{
+	{ /*parent process*/
 		wait(&(inf->status));
+		/*Check if the child process exited normally:*/
 		if (WIFEXITED(inf->status))
 		{
 			inf->status = WEXITSTATUS(inf->status);
 			if (inf->status == 126)
+			{
 				print_error(inf, "Permission denied\n");
+			}
 		}
 	}
 }
